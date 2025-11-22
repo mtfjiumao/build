1d273e75a7a679d28c3a316df18c45c256a7c3b9
九猫ovo <139616353+mtfjiumao@users.noreply.github.com>
2025-11-22 12:22:59 +0800

    Update rock5b.mk

--- rock5b.mk
+++ rock5b.mk
@@ -23,7 +23,7 @@ BOOT_IMG		?= $(ROOT)/out/rock5b.img
 RKBIN_PATH ?= $(ROOT)/rkbin
 RKDEVELOPTOOL_PATH	?= $(ROOT)/rkdeveloptool
 RKDEVELOPTOOL_BIN	?= $(RKDEVELOPTOOL_PATH)/rkdeveloptool
-LOADER_BIN		?= $(BINARIES_PATH)/rk3588_spl_loader_v1.15.113.bin
+LOADER_BIN		?= $(BINARIES_PATH)/rk3588_spl_v1.13.bin
 TPL_BIN		        ?= $(BINARIES_PATH)/rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.19.bin
 
 LINUX_MODULES ?= y
